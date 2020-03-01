6b0320309ecd0ca97fbfdf5b1b92ee8136b760b9
Melanie ZHU <mzhu@e1r13p4.42.fr>
2020-03-01 16:13:06 +0100

    segfautl everywhere

---
+++ working on/srcs/split_line.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   split_line.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzhu <mzhu@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/02 16:16:33 by mzhu              #+#    #+#             */
+/*   Updated: 2020/02/29 14:32:52 by mzhu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fdf.h>
+
+int				*split_line(char *str, int c, int size_line)
+{
+	int			*tab;
+	int			ind;
+	int			result;
+	int			nb_int;
+	int			pos;
+
+	pos = 0;
+	ind = 0;
+	result = 0;
+	nb_int = ft_count_word(str, ' ');
+	if (!(tab = (int*)malloc(sizeof(int) * size_line + 1)))
+		return (NULL);
+	while (str[ind])
+	{
+		while (str[ind] == c && str[ind])
+			ind++;
+		if (str[ind] != c && str[ind])
+			tab[pos++] = ft_atoi(str + ind);
+		while (str[ind] != c && str[ind])
+			ind++;
+	}
+	while (size_line > pos)
+		tab[pos++] = 0;
+	return (tab);
+}
